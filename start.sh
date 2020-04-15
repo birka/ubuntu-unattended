87b1ffb55dc4cd3a4e0c69c2cec0577ef1adf3d3
Alexander Birk <abirk@pingworks.de>
2020-04-15 16:05:10 +0200

    start doesn't need to set hostname anymore

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# set defaults
-default_hostname="$(hostname)"
-default_domain="$(hostname -d)"
-tmp="/root/"
-
 clear
 
 # check for root privilege
@@ -13,15 +8,6 @@ if [ "$(id -u)" != "0" ]; then
    echo " this script must be run as root" 1>&2
    echo
    exit 1
-fi
-
-# check for interactive shell
-if ! grep -q "noninteractive" /proc/cmdline ; then
-    stty sane
-
-    # ask questions
-    read -ep " please enter your preferred hostname: " -i "$default_hostname" hostname
-    read -ep " please enter your preferred domain: " -i "$default_domain" domain
 fi
 
 # print status message
@@ -42,15 +28,6 @@ for P in $(fdisk -l /dev/sda | grep 'Linux filesystem' | awk '{print $1;}');do
 done
 mount $FREE_UNMOUNTED_PART /var/lib/docker
 echo "$FREE_UNMOUNTED_PART	/var/lib/docker	ext4	defaults	0	0" >> /etc/fstab
-
-# set fqdn
-fqdn="$hostname.$domain"
-
-# update hostname
-echo "$hostname" > /etc/hostname
-sed -i "s@ubuntu.ubuntu@$fqdn@g" /etc/hosts
-sed -i "s@ubuntu@$hostname@g" /etc/hosts
-hostname "$hostname"
 
 # update repos
 apt-get -y update
