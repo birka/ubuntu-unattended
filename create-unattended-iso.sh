39ecbddc561677e13d16747a9b6bba7943a5d44e
Alexander Birk <abirk@pingworks.de>
2020-05-22 12:42:36 +0200

    WIP

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-hostname="node04"
+hostname="node01"
 domainname="k8s.la.pingworks.net"
-ipv4_address="192.168.22.104"
+ipv4_address="192.168.22.101"
 ipv4_network="255.255.255.0"
 ipv4_gateway="192.168.22.2"
 dns_servers="192.168.22.2"
 
-#ubuntu_version="18.04.4"
-ubuntu_version="20.04"
+ubuntu_version="18.04.4"
+# ubuntu_version="20.04"
 
 # file names & paths
 tmp="/var/tmp"  # destination folder to store the final iso file
@@ -75,89 +75,10 @@ case "$(lsb_release -rs)" in
     *) ub1604="" ;;
 esac
 
-# get the latest versions of Ubuntu LTS
-# COMMENT IN after 20.04 release
-
-# tmphtml=$tmp/tmphtml
-# rm $tmphtml >/dev/null 2>&1
-# wget -O $tmphtml 'http://releases.ubuntu.com/' >/dev/null 2>&1
-
-# # create the menu based on available versions from
-# # http://cdimage.ubuntu.com/releases/
-# # http://releases.ubuntu.com/
-
-# WORKFILE=www.list
-# EXCLUDE_LIST='torrent|zsync|live'
-# COUNTER=1
-# if [ ! -z $1 ] && [ $1 == "rebuild" ]; then
-#     rm -f ${WORKFILE}
-# fi
-# if [ ! -e ${WORKFILE} ]; then
-#      echo Building menu from available builds
-#      for version in $(wget -qO - http://cdimage.ubuntu.com/releases/ | grep -w DIR | grep -oP href=\"[0-9].* | cut -d'"' -f2 | tr -d '/'); do
-#         TITLE=$(wget -qO - http://cdimage.ubuntu.com/releases/${version}/release | grep h1 | sed s'/^ *//g' | sed s'/^.*\(Ubuntu.*\).*$/\1/' | sed s'|</h1>||g')
-#         CODE=$(echo ${TITLE} | cut -d "(" -f2 | tr -d ")")
-#         URL=http://releases.ubuntu.com/${version}/
-#         wget -qO - ${URL} | grep server | grep amd64 | egrep -v ${EXCLUDE_LIST} > /dev/null
-#         if [ $? -ne 0 ] ; then
-#             URL=http://cdimage.ubuntu.com/releases/${version}/release/
-#         fi
-#         FILE=$(wget -qO - ${URL} | grep server-amd64 | grep -o ubuntu.*.iso | egrep -v ${EXCLUDE_LIST} | grep ">" | cut -d ">" -f2 | sort -u)
-#         FILE=$(echo ${FILE} | tr "\n" " " | tr "\r" " ")
-#         echo "FILE:$FILE"
-#         if [[ ! -z ${FILE} ]]; then
-#             echo ${TITLE}
-#             for iso in ${FILE}; do
-#                 ver=$(echo ${iso} | cut -d- -f2)
-#                 if [ ! -e ${WORKFILE} ] || ! grep -q "${ver} " ${WORKFILE}; then
-#                     echo "${COUNTER} ${ver} ${URL} ${iso} \"${CODE}\"" >> ${WORKFILE}
-#                     ((COUNTER++))
-#                 fi
-#             done
-#         fi
-#      done | uniq
-# fi
-
-# # display the menu for user to select version
-# echo hier
-# MIN=1
-# MAX=$(tail -1 ${WORKFILE} | awk '{print $1}')
-# ubver=0
-# while [ ${ubver} -lt ${MIN} ] || [ ${ubver} -gt ${MAX} ]; do
-#     echo " which ubuntu edition would you like to remaster:"
-#     echo
-#     cat ${WORKFILE} | while read A B C D E; do
-#         echo " [$A] Ubuntu $B ($E)"
-#     done
-#     echo
-#     read -p " please enter your preference: [${MIN}-${MAX}]: " ubver
-# done
-
-# download_file=$(grep -w ^$ubver ${WORKFILE} | awk '{print $4}')           # filename of the iso to be downloaded
-# download_location=$(grep -w ^$ubver ${WORKFILE} | awk '{print $3}')     # location of the file to be downloaded
-# new_iso_name="ubuntu-$(grep -w ^$ubver ${WORKFILE} | awk '{print $2}')-server-amd64-unattended.iso" # filename of the new iso file to be created
-# ---
-
-# COMMENT OUT after 20.04 release
-download_file="ubuntu-20.04-server-amd64.iso"
-new_iso_name="ubuntu-20.04-server-amd64-unattended.iso"
-# ----
-
-if [ -f /etc/timezone ]; then
-  timezone=`cat /etc/timezone`
-elif [ -h /etc/localtime ]; then
-  timezone=`readlink /etc/localtime | sed "s/\/usr\/share\/zoneinfo\///"`
-else
-  checksum=`md5sum /etc/localtime | cut -d' ' -f1`
-  timezone=`find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$checksum" | sed "s/.*\/usr\/share\/zoneinfo\///" | head -n 1`
-fi
-
-# ask the user questions about his/her preferences
+download_file="ubuntu-18.04-server-amd64.iso"
+new_iso_name="ubuntu-18.04-server-amd64-unattended.iso"
 username="root"
 timezone="Europe/Berlin"
-# generate the password hash
-# pwhash=$(echo $password | mkpasswd -s -m sha-512)
-
 pwhash='$6$FHn1fCc0$qCp.HOa/LKayMTydkE6bbzYj5XEz40rQyJqBsJkq8lFYX4JsrUsuYWbEaPG8NcPGMR.KlbeD4iJHrUX6Hu4M30'
 bootable="yes"
 
